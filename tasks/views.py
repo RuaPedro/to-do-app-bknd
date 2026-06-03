39954f44947e93542e4944bf968a526ecbf93fd8
RuaPedro <pedroruaaa@gmail.com>
2026-06-02 20:55:09 -0700

    ci: add lint workflow and intentional flake8 errors for demo

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,18 @@
+import datetime
+
 from rest_framework import viewsets
+
 from .models import Task, SubTask, Comment
 from .serializers import TaskSerializer, SubTaskSerializer, CommentSerializer
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def list(self, request, *args, **kwargs):
+        unused_var = "test"
+        return super().list(request, *args, **kwargs)
 
 class SubTaskViewSet(viewsets.ModelViewSet):
     queryset = SubTask.objects.all()
